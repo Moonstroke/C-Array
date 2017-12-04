1a072aec9a1b6117cae1e81e205fb8619accaa59
joH1 <jiema@live.fr>
2017-12-04 14:56:58 +0100

    changed tests structure, made variable `i` local

--- test.c
+++ test.c
@@ -17,71 +17,88 @@ int main(const int argc, char *const argv[]) {
 
 	LinkedList l = newlinkedlist();
 
-	int ints[] = {42, 3, 7, 13};
+	const int ints[] = {42, 3, 7, 13};
 	const unsigned int N = 4;
-	unsigned int i;
 
 	info("tests lappend");
-	for(i = 0; i < N; ++i) {
-		int v = ints[i];
-		ssize_t got, expected;
+	{
+		unsigned int i;
+		for(i = 0; i < N; ++i) {
+			int v = ints[i];
+			ssize_t got, expected;
 
-		info("lappend(l, %d)", v);
-		expected = i;
-		verbose("expected: %ld", expected);
-		got = lappend(l, (void*)&ints[i]);
-		verbose("got     : %ld", got);
-		assert(got == expected);
-		info("OK");
+			info("lappend(l, %d)", v);
+			expected = i;
+			verbose("expected: %ld", expected);
+			got = lappend(l, (void*)&ints[i]);
+			verbose("got     : %ld", got);
+			assert(got == expected);
+			info("OK");
+		}
+
 		printf("list = ");
 		lprintf(l, &printint);
 	}
 
 	info("\ntests get");
-	for(i = 0; i < N; ++i) {
-		int got, expected;
-		expected = ints[i];
-		verbose("expected: %d", expected);
-		got = *((int*)lget(l, i));
-		verbose("got     : %d", got);
-		assert(got == expected);
-		info("OK");
+	{
+		unsigned int i;
+		for(i = 0; i < N; ++i) {
+			int got, expected;
+			expected = ints[i];
+			verbose("expected: %d", expected);
+			got = *((int*)lget(l, i));
+			verbose("got     : %d", got);
+			assert(got == expected);
+			info("OK");
+		}
 	}
 
 	info("\ntests add");
-	i = 3;
-	int add_extra = 8;
-	int got, expected = i;
-	verbose("expected: %d", expected);
-	got = ladd(l, i, &add_extra);
-	verbose("got     : %d", got);
-	assert(got == expected);
-	info("OK");
-	printf("list = ");
-	lprintf(l, &printint);
+	{
+		unsigned int i = 3;
+		int extra = 8;
+		int got, expected = i;
+		verbose("expected: %d", expected);
+		got = ladd(l, i, &extra);
+		verbose("got     : %d", got);
+		assert(got == expected);
+		info("OK");
+
+		printf("list = ");
+		lprintf(l, &printint);
+	}
 
 	info("\ntests set");
-	i = 2;
-	int set_extra = -32;
-	expected = ints[i];
-	verbose("expected: %d\n", expected);
-	got = *((int*)lset(l, i, &set_extra));
-	verbose("got     : %d", got);
-	assert(got == expected);
-	info("OK");
-	printf("list = ");
-	lprintf(l, &printint);
+	{
+		unsigned int i = 2;
+		int extra = -32;
+		int expected, got;
+		expected = ints[i];
+		verbose("expected: %d", expected);
+		got = *((int*)lset(l, i, &extra));
+		verbose("got     : %d", got);
+		assert(got == expected);
+		info("OK");
+
+		printf("list = ");
+		lprintf(l, &printint);
+	}
 
 	info("\ntests drop");
-	i = 0;
-	expected = ints[0];
-	verbose("expected: %d", expected);
-	got = *((int*)ldrop(l, i));
-	verbose("got     : %d", got);
-	assert(got == expected);
-	info("OK");
-	printf("list = ");
-	lprintf(l, &printint);
+	{
+		unsigned int i = 0;
+		int expected, got;
+		expected = ints[0];
+		verbose("expected: %d", expected);
+		got = *((int*)ldrop(l, i));
+		verbose("got     : %d", got);
+		assert(got == expected);
+		info("OK");
+
+		printf("list = ");
+		lprintf(l, &printint);
+	}
 
 	info("\n test free");
 	lfree(l, NULL);
