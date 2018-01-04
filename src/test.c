423dd00d1c6e20cbcf22dd01a4b6337a9e97c6f7
joH1 <jiema@live.fr>
2018-01-04 15:24:01 +0100

    Move var decls to higher scope
    This resolve the behavior when two variables in two different blocks
    ended up living in the same memory address.

--- src/test.c
+++ src/test.c
@@ -22,13 +22,15 @@ int main(void) {
 	const int ints[] = {42, 3, 7, 13};
 	const unsigned int N = 4;
 
+	/* Variables used in tests */
+	unsigned int i;
+	int extra_add, extra_set;
+
 	info("tests lappend");
 	{
-		unsigned int i;
 		for(i = 0; i < N; ++i) {
 			int v = ints[i];
 			int got, expected;
-
 			info("lappend(l, %d)", v);
 			expected = i;
 			verbose("expected: %ld", expected);
@@ -44,7 +46,6 @@ int main(void) {
 
 	info("\ntests get");
 	{
-		unsigned int i;
 		for(i = 0; i < N; ++i) {
 			int got, expected;
 			expected = ints[i];
@@ -58,27 +59,26 @@ int main(void) {
 
 	info("\ntests add");
 	{
-		unsigned int i = 3;
-		int extra = 8;
 		int got, expected = i;
+		i = 3;
+		extra_add = 8;
 		verbose("expected: %d", expected);
-		got = ladd(l, i, &extra);
+		got = ladd(l, i, &extra_add);
 		verbose("got     : %d", got);
 		assert(got == expected);
 		info("OK");
-
 		printf("list = ");
 		lprintf(l, &printint);
 	}
 
 	info("\ntests set");
 	{
-		unsigned int i = 2;
-		int extra = -32;
-		int expected, got;
+		int got, expected;
+		i = 2;
+		extra_set = -32;
 		expected = ints[i];
 		verbose("expected: %d", expected);
-		got = *((int*)lset(l, i, &extra));
+		got = *((int*)lset(l, i, &extra_set));
 		verbose("got     : %d", got);
 		assert(got == expected);
 		info("OK");
@@ -89,8 +89,8 @@ int main(void) {
 
 	info("\ntests drop");
 	{
-		unsigned int i = 0;
 		int expected, got;
+		i = 0;
 		expected = ints[i];
 		verbose("expected: %d", expected);
 		got = *((int*)ldrop(l, i));
