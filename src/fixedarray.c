c877be922c6d961a924499b956bcad1a9875a684
joH1 <jiema@live.fr>
2018-01-28 15:55:21 +0100

    Add implementation for the FixedArray functions
    The functions are implemented to pass the adequate tests and follow the
    behaviour they were designed for.

---
+++ src/fixedarray.c
@@ -0,0 +1,46 @@
+#include "fixedarray.h"
+
+#include <stdlib.h> /* for NULL, malloc(), calloc(), free() */
+
+
+
+struct fixedarray {
+	data **items;
+	unsigned int size;
+	char _padding[4];
+};
+
+
+FixedArray *fa_new(const unsigned int s) {
+	if(!s)
+		return NULL;
+	FixedArray *const fa = malloc(sizeof(FixedArray));
+	if(!fa)
+		return NULL;
+	data **const i = calloc(s, sizeof(data*));
+	if(!i) {
+		free(fa);
+		return NULL;
+	}
+	fa->size = s;
+	fa->items = i;
+	return fa;
+}
+
+void fa_free(FixedArray *const fa) {
+	free(fa->items);
+	free(fa);
+}
+
+
+unsigned int fa_size(const FixedArray *const fa) {
+	return fa->size;
+}
+
+data *fa_get(const FixedArray *const fa, const unsigned int i) {
+	return fa->items[i];
+}
+
+void fa_set(FixedArray *const fa, const unsigned int i, data *const d) {
+	fa->items[i] = d;
+}
