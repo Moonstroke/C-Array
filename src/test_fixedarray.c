7116a5703193f90e4a7c2bb8a3c62fb5d1b44fb3
joH1 <jiema@live.fr>
2018-01-28 18:45:08 +0100

    Add test set for FixedArray extra functions
    The suite of tests checks the running of the functions specified in
    `fixedarray_funcs.h`.

--- src/test_fixedarray.c
+++ src/test_fixedarray.c
@@ -10,6 +10,19 @@
 static FixedArray *farray;
 static const unsigned int test_farray_size = 8;
 static int values[] = {1, 42, 6, 3, 27, 9, 55, 700};
+static const unsigned int unset_index = 3;
+static int extra_value = 33;
+static void inc(data *const v) {
+	++*(int*)v;
+}
+static const int values_swap_inc[] = {2, 43, 7, 34, 28, 10, 56, 701};
+static const char *const fa_printf_output = "[2, 43, 7, 34, 28, 10, 56, 701]";
+static void print_as_int(const data *const v) {
+	if(v)
+		printf("%d", *(int*)v);
+	else
+		printf("(null)");
+}
 
 
 void test_fixedarray(void) {
@@ -66,6 +79,60 @@ void test_fixedarray(void) {
 			assert(got == expected);
 		}
 		info("OK\n");
+	}
+
+	{
+		int expected, got;
+		info("test fa_unset(%u)", unset_index);
+		expected = values[unset_index];
+		verbose("Expected: %d", expected);
+		got = *(int*)fa_unset(farray, unset_index);
+		verbose("Got     : %d", got);
+		assert(got == expected);
+		info("OK\n");
+	}
+
+	{
+		unsigned int expected, got;
+		info("test fa_count()");
+		expected = test_farray_size - 1;
+		verbose("Expected: %u", expected);
+		got = fa_count(farray);
+		verbose("Got     : %u", got);
+		assert(got == expected);
+		info("OK\n");
+	}
+
+	{
+		unsigned int expected, got;
+		info("test fa_put(%d)", extra_value);
+		expected = unset_index;
+		verbose("Expected: %u", expected);
+		got = fa_put(farray, (data*)&extra_value);
+		verbose("Got     : %u", got);
+		assert(got == expected);
+		info("OK\n");
+	}
+
+	{
+		info("test fa_each()");
+		fa_each(farray, inc);
+		verbose("Checking array elements...");
+		for(unsigned int i = 0; i < test_farray_size; ++i) {
+			const int expected = values_swap_inc[i],
+			          got = *(int*)fa_get(farray, i);
+			assert(got == expected);
+			verbose("OK for %u", i);
+		}
+		info("OK\n");
+	}
+
+	{
+		info("test fa_printf()");
+		verbose("Expected output is:\n%s", fa_printf_output);
+		verbose("Output got is:");
+		fa_printf(farray, print_as_int);
+		info("Please check manually.\n");
 	}
 
 	info("fa_free");
