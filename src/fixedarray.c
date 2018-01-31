427265c25ab11cefc90576a9737d67cd78b83876
joH1 <jiema@live.fr>
2018-01-31 19:17:06 +0100

    Use flexible array member
    The double pointer to store the elements is replaced with a flexible
    array member, that is an array defined without a size that is sized
    when the containing structure is allocated.
    There are limitations though: the structure has to be allocated
    dynamically (*malloc()* n' stuff) and the array must be the last member
    in the definition of the structure.

--- src/fixedarray.c
+++ src/fixedarray.c
@@ -2,34 +2,29 @@
 
 #include <stdint.h> /* for uint8_t */
 #include <stdlib.h> /* for NULL, malloc(), calloc(), free() */
+#include <string.h> /* for memset() */
 
 
 
 struct fixedarray {
-	data **items;
 	unsigned int size;
 	uint8_t _padding[4];
+	data *items[];
 };
 
 
 FixedArray *fa_new(const unsigned int s) {
 	if(!s)
 		return NULL;
-	FixedArray *const fa = malloc(sizeof(FixedArray));
+	FixedArray *const fa = malloc(sizeof(FixedArray) + s * sizeof(data*));
 	if(!fa)
 		return NULL;
-	data **const i = calloc(s, sizeof(data*));
-	if(!i) {
-		free(fa);
-		return NULL;
-	}
 	fa->size = s;
-	fa->items = i;
+	memset(fa->items, 0, s);
 	return fa;
 }
 
 void fa_free(FixedArray *const fa) {
-	free(fa->items);
 	free(fa);
 }
 
