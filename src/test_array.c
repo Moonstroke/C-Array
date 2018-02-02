969e8133d4c78f60f5a3ec36df3cd3d6ee121e93
joH1 <jiema@live.fr>
2018-02-02 15:17:53 +0100

    Refactor tests
    The tests have all been dispatched in independent `static` procedures,
    that in turn are called from the function defining the test suite.

--- src/test_array.c
+++ src/test_array.c
@@ -6,120 +6,137 @@
 #include <stdio.h>
 
 
-#define INT_ARRAY_SIZE 10
 
-static void printint(const void *i) {
-	if(i == NULL)
-		printf("(nil)");
-	else
-		printf("%d", *(int*)i);
+static Array *array;
+
+static const unsigned int INT_ARRAY_SIZE = 10;
+static int values[] = {-1, 42, 666, 13, 28, -54, 0, 7 , 6, 5};
+
+
+static void init(void) {
+	info("Initialization of array of %u items", INT_ARRAY_SIZE);
+	array = a_new(INT_ARRAY_SIZE);
+}
+
+static void cleanup(void) {
+	a_free(array);
+}
+
+
+static void test_a_append(void) {
+	unsigned int i;
+	info("test a_append");
+	verbose("values = [-1, 42, 666, 13, 28, -54, 0, 7, 6, 5]");
+	for(i = 0; i < INT_ARRAY_SIZE; ++i) {
+		int expected, got;
+		expected = i;
+		verbose("a_append(array, %d)", values[i]);
+		verbose("expected: %ld", expected);
+		got = a_append(array, &values[i]);
+		verbose("got     : %ld", got);
+		assert(got == expected);
+	}
+	info("OK\n");
+}
+
+static void test_a_get(void) {
+	info("tests a_get");
+	for(unsigned int i = 0; i < INT_ARRAY_SIZE; ++i) {
+		int expected, got;
+		expected = values[i];
+		verbose("a_get(array, %d)", i);
+		verbose("expected: %d", expected);
+		got = *((int*)a_get(array, i));
+		verbose("got     : %d", got);
+		assert(got == expected);
+	}
+	info("OK\n");
+}
+
+static void test_a_append__overflow(void) {
+	static int extra = 73;
+	int expected, got;
+	info("test a_append - overflow size (=> realloc)");
+	expected = INT_ARRAY_SIZE;
+	verbose("a_append(array, %d)", extra);
+	verbose("expected: %ld", expected);
+	got = a_append(array, &extra);
+	verbose("got     : %ld", got);
+	assert(got == expected);
+	info("OK\n");
+}
+
+void test_a_set(void) {
+	static int extra = 777;
+	int expected, got;
+	unsigned int i = 2;
+	info("test a_set");
+	expected = values[i];
+	verbose("a_set(array, %u, %d)", i, extra);
+	verbose("expected: %ld", expected);
+	got = *(int*)a_set(array, i, &extra);
+	verbose("got     : %ld", got);
+	assert(got == expected);
+	info("OK\n");
 }
 
 static bool equals42(const void *const e) {
 	assert(e != NULL);
 	return *((int*)e) == 42;
 }
+static void test_a_cond(void) {
+	int expected, got;
+	info("test a_cond");
+	expected = 42;
+	verbose("acond(array, %s)", "(int i) -> (i == 42)"); // string repr of the equals42() func
+	verbose("expected = %d", expected);
+	got = *(int*)a_cond(array, equals42);
+	verbose("got      = %d", got);
+	assert(got == expected);
+	info("OK\n");
+}
 
 static bool equals1024(const void *const e) {
 	assert(e != NULL);
 	return *((int*)e) == 1024;
 }
-
-
-void test_array(void) {
-	// 10 integers array
-	Array *a = a_new(INT_ARRAY_SIZE);
-
-	int ints[] = {-1, 42, 666, 13, 28, -54, 0, 7 , 6, 5};
-	verbose("values = [-1, 42, 666, 13, 28, -54, 0, 7, 6, 5]");
-
-	info("test append");
-	{
-		unsigned int i;
-		for(i = 0; i < INT_ARRAY_SIZE; ++i) {
-			int expected, got;
-			expected = i;
-			verbose("a_append(a, %d)", ints[i]);
-			verbose("expected: %ld", expected);
-			got = a_append(a, &ints[i]);
-			verbose("got     : %ld", got);
-			assert(expected == got);
-		}
-	}
-	a_printf(a, *printint);
-	info("OK\n");
-
-	info("tests get");
-	{
-		unsigned int i;
-		for(i = 0; i < INT_ARRAY_SIZE; ++i) {
-			int expected, got;
-			expected = ints[i];
-			verbose("a_get(a, %d)", i);
-			verbose("expected: %d", expected);
-			got = *((int*)a_get(a, i));
-			verbose("got     : %d", got);
-			assert(got == expected);
-		}
-	}
-	a_printf(a, *printint);
-	info("OK\n");
-
-	info("test append - overflow size (=> realloc)");
-	{
-		int extra = 73;
-		int expected, got;
-		expected = INT_ARRAY_SIZE;
-		verbose("a_append(a, %d)", extra);
-		verbose("expected: %ld", expected);
-		got = a_append(a, &extra);
-		verbose("got     : %ld", got);
-		assert(expected == got);
-	}
-	a_printf(a, *printint);
-	info("OK\n");
-
-	info("test set");
-	{
-		int extra = 777;
-		int expected, got;
-		unsigned int i = 2;
-		expected = ints[i];
-		verbose("a_set(a, %u, %d)", i, extra);
-		verbose("expected: %ld", expected);
-		got = *(int*)a_set(a, i, &extra);
-		verbose("got     : %ld", got);
-		assert(expected == got);
-	}
-	a_printf(a, *printint);
-	info("OK\n");
-
-	info("test a_cond");
-	{
-		int expected, got;
-		expected = 42;
-		verbose("acond(a, %s)", "(int i) -> (i == 42)"); // string repr of the equals42() func
-		verbose("expected = %d", expected);
-		got = *(int*)a_cond(a, equals42);
-		verbose("got      = %d", got);
-		assert(expected == got);
-	}
-	info("OK\n");
-
+static void test_a_cond__not_found(void) {
+	int *expected, *got;
 	info("test a_cond - not found");
-	{
-		int *expected, *got;
-		expected = NULL;
-		verbose("a_cond(a, %s)", "(int i) -> (i == 1024)");
-		verbose("expected = %p", expected);
-		got = (int*)a_cond(a, equals1024);
-		verbose("got      = %p", got);
-		assert(expected == got);
-	}
+	expected = NULL;
+	verbose("a_cond(array, %s)", "(int i) -> (i == 1024)");
+	verbose("expected = %p", expected);
+	got = (int*)a_cond(array, equals1024);
+	verbose("got      = %p", got);
+	assert(got == expected);
 	info("OK\n");
+}
 
-	info("test a_free");
-	verbose("a_free(a)");
-	a_free(a);
-	info("OK\n");
+
+static void printint(const void *const i) {
+	if(i == NULL)
+		printf("(nil)");
+	else
+		printf("%d", *(int*)i);
+}
+void test_array(void) {
+
+	init();
+
+	test_a_append();
+	a_printf(array, *printint);
+
+	test_a_get();
+
+	test_a_append__overflow();
+	a_printf(array, *printint);
+
+	test_a_set();
+	a_printf(array, *printint);
+
+	test_a_cond();
+
+	test_a_cond__not_found();
+
+	cleanup();
 }
--- src/test_fixedarray.c
+++ src/test_fixedarray.c
@@ -8,134 +8,158 @@
 
 
 static FixedArray *farray;
-static const unsigned int test_farray_size = 8;
+
+static const unsigned int INT_FIXED_ARRAY_SIZE = 8;
 static int values[] = {1, 42, 6, 3, 27, 9, 55, 700};
+
+/* This value must be declared at global level, because it is used
+   in fa_unset AND fa_put */
 static const unsigned int unset_index = 3;
-static int extra_value = 33;
+
+
+static void init(void) {
+	info("construct a FixedArray: fa_new(%u)", INT_FIXED_ARRAY_SIZE);
+	farray = fa_new(INT_FIXED_ARRAY_SIZE);
+	assert(farray != NULL);
+	info("OK\n");
+}
+
+static void cleanup(void) {
+	fa_free(farray);
+}
+
+
+static void test_fa_new__0_null(void) {
+	FixedArray *expected, *got;
+	info("test fa_new(%u)", 0);
+	expected = NULL;
+	verbose("Expected: %p", expected);
+	got = fa_new(0);
+	verbose("Got     : %p", got);
+	assert(fa_new(0) == NULL);
+	info("OK\n");
+}
+
+static void test_fa_size(void) {
+	unsigned int expected, got;
+	info("test fa_size()");
+	expected = INT_FIXED_ARRAY_SIZE;
+	verbose("Expected: %u", expected);
+	got = fa_size(farray);
+	verbose("Got     : %u", got);
+	assert(got == expected);
+	info("OK\n");
+}
+
+static void test_fa_set(void) {
+	info("tests fa_set");
+	for(unsigned int i = 0; i < INT_FIXED_ARRAY_SIZE; ++i) {
+		data *const val = &values[i];
+		verbose("fa_set(%u, %p)", i, val);
+		fa_set(farray, i, val);
+	}
+	info("OK\n");
+}
+
+static void test_fa_get(void) {
+	info("tests fa_get");
+	for(unsigned int i = 0; i < INT_FIXED_ARRAY_SIZE; ++i) {
+		int expected, got;
+		verbose("fa_get(%u)", i);
+		expected = values[i];
+		verbose("Expected: %d", expected);
+		got = *(int*)fa_get(farray, i);
+		verbose("Got     : %d", got);
+		assert(got == expected);
+	}
+	info("OK\n");
+}
+
+static void test_fa_unset(void) {
+	int expected, got;
+	info("test fa_unset(%u)", unset_index);
+	expected = values[unset_index];
+	verbose("Expected: %d", expected);
+	got = *(int*)fa_unset(farray, unset_index);
+	verbose("Got     : %d", got);
+	assert(got == expected);
+	info("OK\n");
+}
+
+static void test_fa_count(void) {
+	unsigned int expected, got;
+	info("test fa_count()");
+	expected = INT_FIXED_ARRAY_SIZE - 1;
+	verbose("Expected: %u", expected);
+	got = fa_count(farray);
+	verbose("Got     : %u", got);
+	assert(got == expected);
+	info("OK\n");
+}
+
+static void test_fa_put(void) {
+	static int extra_value = 33;
+	unsigned int expected, got;
+	info("test fa_put(%d)", extra_value);
+	expected = unset_index;
+	verbose("Expected: %u", expected);
+	got = fa_put(farray, &extra_value);
+	verbose("Got     : %u", got);
+	assert(got == expected);
+	info("OK\n");
+}
+
 static void inc(data *const v) {
 	++*(int*)v;
 }
-static const int values_swap_inc[] = {2, 43, 7, 34, 28, 10, 56, 701};
-static const char *const fa_printf_output = "[2, 43, 7, 34, 28, 10, 56, 701]";
+static void test_fa_each(void) {
+	const int values_after[] = {2, 43, 7, 34, 28, 10, 56, 701};
+	info("test fa_each()");
+	fa_each(farray, inc);
+	verbose("Checking array elements...");
+	for(unsigned int i = 0; i < INT_FIXED_ARRAY_SIZE; ++i) {
+		const int expected = values_after[i],
+		          got = *(int*)fa_get(farray, i);
+		assert(got == expected);
+		verbose("OK for %u", i);
+	}
+	info("OK\n");
+}
+
+
 static void print_as_int(const data *const v) {
 	if(v)
 		printf("%d", *(int*)v);
 	else
 		printf("(null)");
 }
-
-
 void test_fixedarray(void) {
 
 	log_setfilter(LOG_VERBOSE);
 
+	init();
 
-	info("construct a FixedArray: fa_new(%u)", test_farray_size);
-	farray = fa_new(test_farray_size);
-	assert(farray != NULL);
-	info("OK\n");
 
-	{
-		FixedArray *expected, *got;
-		info("test fa_new(%u)", 0);
-		expected = NULL;
-		verbose("Expected: %p", expected);
-		got = fa_new(0);
-		verbose("Got     : %p", got);
-		assert(fa_new(0) == NULL);
-		info("OK\n");
-	}
+	test_fa_new__0_null();
 
-	{
-		unsigned int expected, got;
-		info("test fa_size()");
-		expected = test_farray_size;
-		verbose("Expected: %u", expected);
-		got = fa_size(farray);
-		verbose("Got     : %u", got);
-		assert(got == expected);
-		info("OK\n");
-	}
+	test_fa_size();
 
-	{
-		info("tests fa_set");
-		for(unsigned int i = 0; i < test_farray_size; ++i) {
-			data *const val = &values[i];
-			verbose("fa_set(%u, %p)", i, val);
-			fa_set(farray, i, val);
-		}
-		info("OK\n");
-	}
+	test_fa_set();
+	fa_printf(farray, print_as_int);
 
-	{
-		info("tests fa_get");
-		for(unsigned int i = 0; i < test_farray_size; ++i) {
-			int expected, got;
-			verbose("fa_get(%u)", i);
-			expected = values[i];
-			verbose("Expected: %d", expected);
-			got = *(int*)fa_get(farray, i);
-			verbose("Got     : %d", got);
-			assert(got == expected);
-		}
-		info("OK\n");
-	}
+	test_fa_get();
 
-	{
-		int expected, got;
-		info("test fa_unset(%u)", unset_index);
-		expected = values[unset_index];
-		verbose("Expected: %d", expected);
-		got = *(int*)fa_unset(farray, unset_index);
-		verbose("Got     : %d", got);
-		assert(got == expected);
-		info("OK\n");
-	}
 
-	{
-		unsigned int expected, got;
-		info("test fa_count()");
-		expected = test_farray_size - 1;
-		verbose("Expected: %u", expected);
-		got = fa_count(farray);
-		verbose("Got     : %u", got);
-		assert(got == expected);
-		info("OK\n");
-	}
+	test_fa_unset();
+	fa_printf(farray, print_as_int);
 
-	{
-		unsigned int expected, got;
-		info("test fa_put(%d)", extra_value);
-		expected = unset_index;
-		verbose("Expected: %u", expected);
-		got = fa_put(farray, (data*)&extra_value);
-		verbose("Got     : %u", got);
-		assert(got == expected);
-		info("OK\n");
-	}
+	test_fa_count();
 
-	{
-		info("test fa_each()");
-		fa_each(farray, inc);
-		verbose("Checking array elements...");
-		for(unsigned int i = 0; i < test_farray_size; ++i) {
-			const int expected = values_swap_inc[i],
-			          got = *(int*)fa_get(farray, i);
-			assert(got == expected);
-			verbose("OK for %u", i);
-		}
-		info("OK\n");
-	}
+	test_fa_put();
+	fa_printf(farray, print_as_int);
 
-	{
-		info("test fa_printf()");
-		verbose("Expected output is:\n%s", fa_printf_output);
-		verbose("Output got is:");
-		fa_printf(farray, print_as_int);
-		info("Please check manually.\n");
-	}
+	test_fa_each();
+	fa_printf(farray, print_as_int);
 
-	info("fa_free");
-	fa_free(farray);
-	info("OK\n");
+	cleanup();
 }
