9fb3fdd57dc9caf707872cb15f4f9e8478e393a3
joH1 <jiema@live.fr>
2018-02-02 16:44:06 +0100

    Add tests for invalid index in a_get and a_set calls
    The former tests for these functions  have been renamed to indicate
    that they test for valid calls.

--- src/test_array.c
+++ src/test_array.c
@@ -61,14 +61,33 @@ static void test_a_size__full(void) {
 	info("OK\n");
 }
 
-static void test_a_get(void) {
-	info("tests a_get");
+static void test_a_get__valid(void) {
+	info("tests a_get -- valid indices");
 	for(unsigned int i = 0; i < INT_ARRAY_SIZE; ++i) {
 		int expected, got;
 		expected = values[i];
 		verbose("a_get(array, %d)", i);
 		verbose("expected: %d", expected);
 		got = *((int*)a_get(array, i));
+		verbose("got     : %d", got);
+		assert(got == expected);
+	}
+	info("OK\n");
+}
+
+static void test_a_get__invalid(void) {
+	const unsigned int invalid_indices[3] = {
+		a_size(array),
+		a_size(array) + 1,
+		73 /* big "arbitrary" value */
+	};
+	info("tests a_get -- invalid indices");
+	for(unsigned int i = 0; i < 3; ++i) {
+		data *expected, *got;
+		expected = NULL;
+		verbose("a_get(array, %u)", invalid_indices[i]);
+		verbose("expected: %p", expected);
+		got = a_get(array, invalid_indices[i]);
 		verbose("got     : %d", got);
 		assert(got == expected);
 	}
@@ -88,17 +107,37 @@ static void test_a_append__overflow(void) {
 	info("OK\n");
 }
 
-void test_a_set(void) {
+void test_a_set__valid(void) {
 	static int extra = 777;
 	int expected, got;
 	unsigned int i = 2;
-	info("test a_set");
+	info("test a_set -- valid indices");
 	expected = values[i];
 	verbose("a_set(array, %u, %d)", i, extra);
 	verbose("expected: %ld", expected);
 	got = *(int*)a_set(array, i, &extra);
 	verbose("got     : %ld", got);
 	assert(got == expected);
+	info("OK\n");
+}
+
+static void test_a_set__invalid(void) {
+	const unsigned int invalid_indices[3] = {
+		a_size(array),
+		a_size(array) + 1,
+		73
+	};
+	static int value = 42;
+	info("tests a_set -- invalid indices");
+	for(unsigned int i = 0; i < 3; ++i) {
+		data *expected, *got;
+		expected = NULL;
+		verbose("a_set(array, %u, %p)", invalid_indices[i]);
+		verbose("expected: %p", expected);
+		got = a_set(array, invalid_indices[i], &value);
+		verbose("got     : %d", got);
+		assert(got == expected);
+	}
 	info("OK\n");
 }
 
@@ -156,13 +195,17 @@ void test_array(void) {
 
 	test_a_size__full();
 
-	test_a_get();
+	test_a_get__valid();
+
+	test_a_get__invalid();
 
 	test_a_append__overflow();
 	a_printf(array, *print_as_int);
 
-	test_a_set();
+	test_a_set__valid();
 	a_printf(array, *print_as_int);
+
+	test_a_set__invalid();
 
 	test_a_cond();
 
