78107fec67d971deeeb304d6a83071a75c152cd5
joH1 <jiema@live.fr>
2018-02-03 16:05:10 +0100

    Cleanup tests for FixedArray
    Extract variables with explicit names, add log messages representing
    the function calls, update type of variables to match the type of the
    represented value: function parameter or return value.

--- src/test_fixedarray.c
+++ src/test_fixedarray.c
@@ -18,7 +18,8 @@ static const unsigned int unset_index = 3;
 
 
 static void init(void) {
-	info("construct a FixedArray: fa_new(%u)", INT_FIXED_ARRAY_SIZE);
+	info("construct a FixedArray");
+	info("fa_new(%u)", INT_FIXED_ARRAY_SIZE);
 	farray = fa_new(INT_FIXED_ARRAY_SIZE);
 	assert(farray != NULL);
 	info("OK\n");
@@ -31,18 +32,20 @@ static void cleanup(void) {
 
 static void test_fa_new__0_null(void) {
 	FixedArray *expected, *got;
+	info("test fa_new -- size of 0 => NULL");
 	info("test fa_new(%u)", 0);
 	expected = NULL;
 	verbose("expected: %p", expected);
 	got = fa_new(0);
 	verbose("got     : %p", got);
-	assert(fa_new(0) == NULL);
+	assert(got == expected);
 	info("OK\n");
 }
 
 static void test_fa_size(void) {
 	unsigned int expected, got;
-	info("test fa_size()");
+	info("test fa_size");
+	info("fa_size(farray)");
 	expected = INT_FIXED_ARRAY_SIZE;
 	verbose("expected: %u", expected);
 	got = fa_size(farray);
@@ -52,43 +55,46 @@ static void test_fa_size(void) {
 }
 
 static void test_fa_set(void) {
-	info("tests fa_set");
-	for(unsigned int i = 0; i < INT_FIXED_ARRAY_SIZE; ++i) {
-		data *const val = &values[i];
-		verbose("fa_set(%u, %p)", i, val);
-		fa_set(farray, i, val);
+	data *param;
+	info("test fa_set");
+	for(unsigned int index = 0; index < INT_FIXED_ARRAY_SIZE; ++index) {
+		param = values + index;
+		info("fa_set(farray, %u, %p)", index, param);
+		fa_set(farray, index, param);
 	}
 	info("OK\n");
 }
 
 static void test_fa_get(void) {
-	info("tests fa_get");
-	for(unsigned int i = 0; i < INT_FIXED_ARRAY_SIZE; ++i) {
-		int expected, got;
-		verbose("fa_get(%u)", i);
-		expected = values[i];
-		verbose("expected: %d", expected);
-		got = *(int*)fa_get(farray, i);
-		verbose("got     : %d", got);
+	data *expected, *got;
+	info("test fa_get");
+	for(unsigned int index = 0; index < INT_FIXED_ARRAY_SIZE; ++index) {
+		info("fa_get(farray, %u)", index);
+		expected = values + index;
+		verbose("expected: %p", expected);
+		got = fa_get(farray, index);
+		verbose("got     : %p", got);
 		assert(got == expected);
 	}
 	info("OK\n");
 }
 
 static void test_fa_unset(void) {
-	int expected, got;
-	info("test fa_unset(%u)", unset_index);
-	expected = values[unset_index];
-	verbose("expected: %d", expected);
-	got = *(int*)fa_unset(farray, unset_index);
-	verbose("got     : %d", got);
+	data *expected, *got;
+	info("test fa_unset");
+	info("fa_unset(farray, %u)", unset_index);
+	expected = values + unset_index;
+	verbose("expected: %p", expected);
+	got = fa_unset(farray, unset_index);
+	verbose("got     : %p", got);
 	assert(got == expected);
 	info("OK\n");
 }
 
 static void test_fa_count(void) {
 	unsigned int expected, got;
-	info("test fa_count()");
+	info("test fa_count");
+	info("fa_count(farray)");
 	expected = INT_FIXED_ARRAY_SIZE - 1;
 	verbose("expected: %u", expected);
 	got = fa_count(farray);
@@ -98,13 +104,14 @@ static void test_fa_count(void) {
 }
 
 static void test_fa_put__valid(void) {
-	static int extra_value = 33;
-	unsigned int expected, got;
-	info("test fa_put(%d)", extra_value);
+	static int extra = 33;
+	data *const param = &extra;
+	int expected, got;
+	info("test fa_put(farray, %d)", param);
 	expected = unset_index;
-	verbose("expected: %u", expected);
-	got = fa_put(farray, &extra_value);
-	verbose("got     : %u", got);
+	verbose("expected: %d", expected);
+	got = fa_put(farray, param);
+	verbose("got     : %d", got);
 	assert(got == expected);
 	info("OK\n");
 }
@@ -126,15 +133,15 @@ static void test_fa_put__invalid(void) {
 static void inc(data *const v) {
 	++*(int*)v;
 }
+static const char inc_repr[] = "(int i) {i++;}";
 static void test_fa_each(void) {
 	const int values_after[] = {2, 43, 7, 34, 28, 10, 56, 701};
-	info("test fa_each()");
+	info("test fa_each");
+	info("fa_each(farray, %s)", inc_repr);
 	fa_each(farray, inc);
 	verbose("Checking array elements...");
 	for(unsigned int i = 0; i < INT_FIXED_ARRAY_SIZE; ++i) {
-		const int expected = values_after[i],
-		          got = *(int*)fa_get(farray, i);
-		assert(got == expected);
+		assert(*(int*)fa_get(farray, i) == values_after[i]);
 		verbose("OK for %u", i);
 	}
 	info("OK\n");
