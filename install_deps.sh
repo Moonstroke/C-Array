b305be912b61398febf63820dac90aeb17263793
joH1 <jiema@live.fr>
2018-02-04 19:36:37 +0100

    Makes dependency installation quieter
    Remove commands output, but add final echo to indicate that the
    function succeeded.

--- install_deps.sh
+++ install_deps.sh
@@ -3,17 +3,19 @@
 # Function declared with round parens is exectued in a subshell
 function install_git_repo() (
 	# Fetch the project
-	git clone -v "$2" "$1" &&
+	git clone --quiet "$2" "$1" &&
 	cd "$1" &&
 	# Build
 	make &&
 	# Install
-	make install
+	make install &&
+	echo "Successfully installed \"$1\" from \"$2\""
 )
 
 function clean_git_repo() {
 	# Delete the repo
-	rm -rfv "$1"
+	rm -rf "$1" &&
+	echo "Removed clone repo \"$1\""
 }
 
 
