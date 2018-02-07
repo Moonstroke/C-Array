af93a8c9ff04f6f6353ecd73c372c25c61d4f975
joH1 <jiema@live.fr>
2018-02-07 19:54:51 +0100

    Implement extra BitArray functions

---
+++ src/bitarray_funcs.c
@@ -0,0 +1,23 @@
+#include "bitarray_funcs.h"
+
+#include <stdio.h>
+
+
+
+unsigned int ba_count(const BitArray *const ba) {
+	unsigned int sum = 0;
+	const unsigned int s = ba_size(ba);
+	for(unsigned int i = 0; i < s; ++i)
+		if(ba_get(ba, i))
+			++sum;
+	return sum;
+}
+
+void ba_printf(const BitArray *const ba) {
+	const unsigned int s = ba_size(ba);
+	printf("[%se", ba_get(ba, 0) ? "tru" : "fals");
+	for(unsigned int i = 1; i < s; ++i) {
+		printf(", %se", ba_get(ba, i) ? "tru" : "fals");
+	}
+	printf("]\n");
+}
