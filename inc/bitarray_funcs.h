b6e2d1467d4a0ada4c0bd47bb088473953b0082a
joH1 <jiema@live.fr>
2018-02-07 19:42:48 +0100

    Add declaration for extra functions for BitArray

---
+++ inc/bitarray_funcs.h
@@ -0,0 +1,15 @@
+#ifndef BITARRAY_FUNCS_H
+#define BITARRAY_FUNCS_H
+
+
+#include "bitarray.h"
+
+
+
+unsigned int ba_count(const BitArray *array);
+
+
+void ba_printf(const BitArray *array);
+
+
+#endif /* BITARRAY_FUNCS_H */
