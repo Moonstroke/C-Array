868f8b3b9788d6015f6d4466a7741c37836b3578
joH1 <jiema@live.fr>
2018-02-17 15:39:32 +0100

    Add doc for BitArray

--- inc/bitarray.h
+++ inc/bitarray.h
@@ -1,3 +1,28 @@
+/**
+ * \file "bitarray.h"
+ * \author joH1
+ * \version 0.1
+ *
+ * \brief A type of array optimized to contain boolean values.
+ *
+ * The type BitArray is a specific type of array whose elements are boolean
+ * values.
+ * It is of fixed size, so it will not operate on memory otherwise than to
+ * instantiate and free the array; it also does not have the ability to add or
+ * remove elements. Indeed this type is designed to express a set of parameters
+ * in a larger architecture, adding or removing any of these parameters has no
+ * actual meaning.
+ *
+ * The functions \a ba_new, \a ba_get, \a ba_put (and hence \a ba_set and
+ * \a ba_unset) set the error variable \a errno, defined in the standard header
+ * \c errno.h to describe the error state of the function.
+ * The variable is assured to be set to \c 0 if the function is in its nominal
+ * state, \c ENOMEM if a memory allocation fails (only for \a ba_new),
+ * \c EINVAL if a given argument has an invalid value (only for \a ba_new as
+ * well, if the size is \c 0) or \c ERANGE if an indexpassed to the function is
+ * invalid (ie. greater or equal to the size of the array).
+ */
+
 #ifndef BITARRAY_H
 #define BITARRAY_H
 
@@ -6,30 +31,98 @@
 
 
 
+/**
+ * \brief A structure that contains a fixed number of binary values.
+ */
 typedef struct bitarray BitArray;
 
 
+/**
+ * \brief Allocates a bit array of given size.
+ *
+ * \note If the allocation fails, the function sets \a errno to \c ENOMEM, or
+ * \c EINVAL if \a size is \c 0.
+ *
+ * \param[in] size The size of the array
+ *
+ * \return A pointer to a bit array of \a size elements, or \c NULL.
+ */
 BitArray *ba_new(unsigned int size);
 
+/**
+ * \brief Releases memory used by the bit array.
+ *
+ * \param[in] self The bit array to free
+ */
 void ba_free(BitArray *self);
 
 
+/**
+ * \brief Gives the number of values held in the array.
+ *
+ * \param[in] self The bit array
+ *
+ * \return The size of the bit array.
+ */
 unsigned int ba_size(const BitArray *self);
 
 
+/**
+ * \brief Retrieves a value from the array at given index.
+ *
+ * \note If the index is invalid, this function sets \a errno to \c ERANGE and
+ *       returns \c false.
+ *
+ * \param[in] self The bit array
+ * \param[in] index The index
+ *
+ * \return The value in the array, at index \a index.
+ */
 bool ba_get(const BitArray *self, unsigned int index);
 
+/**
+ * \brief Gives the element at position \a index in the array the given value.
+ *
+ * \note If the index is invalid, this function sets \a errno to \c ERANGE and
+ *       returns \c false.
+ *
+ * \param[in] self  The bit array
+ * \param[in] index The index
+ * \param[in] value The value to give
+ *
+ * \return The former value of the element, or \c false.
+ */
 bool ba_put(BitArray *self, unsigned int index, bool value);
 
+/**
+ * \brief Sets an element of the bit array to \c true.
+ *
+ * \note If the index is invalid, this function sets \a errno to \c ERANGE and
+ *       returns \c false.
+ *
+ * \param[in] self The bit array
+ * \param[in] index The index
+ *
+ * \return The former value of the element, or \c false.
+ */
 inline bool ba_set(BitArray *const self, const unsigned int index) {
 	return ba_put(self, index, true);
 }
 
+/**
+ * \brief Sets an element of the bit array to \c false.
+ *
+ * \note If the index is invalid, this function sets \a errno to \c ERANGE and
+ *       returns \c false.
+ *
+ * \param[in] self The bit array
+ * \param[in] index The index
+ *
+ * \return The former value of the element, or \c false.
+ */
 inline bool ba_unset(BitArray *const self, const unsigned int index) {
 	return ba_put(self, index, false);
 }
-
-
 
 
 #endif /* BITARRAY_H */
--- inc/bitarray_funcs.h
+++ inc/bitarray_funcs.h
@@ -6,9 +6,25 @@
 
 
 
+/**
+ * \brief Counts the <a href="en.wikipedia.org/wiki/Hamming_weight">Hamming
+ *        weight</a> of the array.
+ *
+ * \param[in] array The bit array
+ *
+ * \return The number of elements set to \c true in the bit array.
+ */
 unsigned int ba_count(const BitArray *array);
 
 
+/**
+ * \brief Outputs the bit array on \a stdout.
+ *
+ * This function prints every element with the exact string \c true or \c false,
+ * separated by commas, and enclosed in square brackets.
+ *
+ * \param[in] array The bit array
+ */
 void ba_printf(const BitArray *array);
 
 
