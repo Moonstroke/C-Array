b9077fdd724105aaffc472e475ed3ebc3332e575
joH1 <jiema@live.fr>
2018-06-23 20:17:37 +0200

    Add inline function sa_geteq
    The function returns an item of the array equivalent to the given value
    as compared with the function passed to `sa_new` when creating the
    array.

--- inc/sortedarray.h
+++ inc/sortedarray.h
@@ -3,6 +3,7 @@
 
 
 #include <stddef.h> /* for size_t */
+#include <stdlib.h> /* for NULL */
 #include <unistd.h> /* for ssize_t */
 
 #include "cods.h" /* for func attrs, data_t */
@@ -24,5 +25,10 @@ CODS_MEMBER data_t *sa_get(const SortedArray *self, size_t index) CODS_PURE;
 
 CODS_MEMBER ssize_t sa_indexof(const SortedArray *self, const data_t *value);
 
+CODS_MEMBER CODS_INLINE CODS_NOTNULL(2) CODS_PURE
+data_t *sa_geteq(const SortedArray *const self, const data_t *other) {
+	const ssize_t index = sa_indexof(self, value);
+	return index == -1 ? NULL : sa_indexof(sa_get(self, index));
+}
 
 #endif /* CODS_SORTEDARRAY_H */
